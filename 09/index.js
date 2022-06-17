78bc921f5b8bff0826b4eb1ccc5769c586a99dc9
Hablu-Programmer <habluprogrammer@gmail.com>
2022-06-17 08:00:47 +0600

    working with atribute

--- 09/index.js
+++ 09/index.js
@@ -1,6 +1,3 @@
-let menu =  document.getElementById("menu"); 
-
-console.log(menu.innerHTML );
-
-let select = document.querySelector("#select");  
-select.innerHTML = "Show Me";
+let menu  = document.querySelector("#menu"); 
+menu.innerHTML = "<a> Click me</a>"
+console.log()
---
+++ 16/index.js
@@ -0,0 +1,26 @@
+let btn = document.querySelector("#btn"); 
+
+// let div = document.querySelector("div"); 
+// div.id = "menu"
+
+
+// setAttribute
+// let btn  = document.querySelector("#btn"); 
+// if(btn){
+//     btn.setAttribute("class", "eshan");
+// } 
+
+
+// getattribute
+//     console.log(btn.getAttribute("class"));
+
+
+// RemoveAttribute
+// let Removebutton  = btn.hasAttribute("target") 
+
+// console.log(Removebutton);
+
+
+//hasAttribute
+
+console.log(btn.hasAttribute("disabled"));
