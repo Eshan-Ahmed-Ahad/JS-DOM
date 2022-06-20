ce99cd89daf84660a39d0a4978a5d71de1010b63
Hablu-Programmer <habluprogrammer@gmail.com>
2022-06-20 06:29:09 +0600

    classlist

---
+++ 20/index.js
@@ -0,0 +1,8 @@
+let menu  =  document.querySelector("#menu");
+menu.classList.add("eshan", "Hablu", "Programmer");
+
+//Remove Child
+menu.classList.remove("eshan","Hablu", "Programmer", "ClassMenu")
+
+//replace child
+menu.classList.replace('ClassMenu', "Info")
