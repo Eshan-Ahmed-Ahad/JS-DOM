5686b6d57126e6ab14c92662ae43df2f8d8bf736
Hablu-Programmer <habluprogrammer@gmail.com>
2023-01-10 17:51:29 +0600

    scroller

--- 27/index.js
+++ 27/index.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-// window.onscroll = () => {
-//   alert("your window is scrolled")
-// }
-
-// window.addEventListener("scroll", ()=> {
-//     console.log("scrolled down")
-// })
 
 let Scroller  = 0;
 
@@ -18,18 +10,3 @@ window.addEventListener("scroll", ()=> {
   }
   Scroller = NewScroller; 
 })
-=======
-var lastScrollTop = 0;
-// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-window.addEventListener("scroll", function() { 
-   let MyScroller = window.pageYOffset || document.documentElement.scrollTop; 
-   if (MyScroller > lastScrollTop){
-       // downscroll code
-		 alert('down');
-   } else {
-      // upscroll code
-		 alert('up');
-   }
-   lastScrollTop = MyScroller;
-}, false);
->>>>>>> 2878586888573787d4e2d91aa3b30802cbc3f52f
